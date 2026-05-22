b345946b4205e04f3449ee79d7aef9a6ce2f55b3
jaajitha <jaajithareddy18@gmail.com>
2026-05-22 09:42:15 +0530

    Remove root frontend files

--- src/api/config.js
+++
@@ -1,9 +0,0 @@
-export const API_CONFIG = {
-  USE_MOCK: true,
-  APPLICATIONS_API:
-    'https://catacomb-stadium-phony.ngrok-free.dev',
-  REDFLAGS_API:
-    'https://eff3-2409-40f0-500a-b398-c93f-f072-d790-8250.ngrok-free.app',
-  MEMO_API:
-    'https://handbag-constrain-blighted.ngrok-free.dev',
-}
