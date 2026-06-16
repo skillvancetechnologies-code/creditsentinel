fb7e9c360081a9bbc4bab73fc0f5bee015aec94c
KOTAREDDY GURU PRASAD SAI <79389012+Guru0685@users.noreply.github.com>
2026-06-16 23:19:06 +0530

    Update red_flag_api.py

--- backend/redflags/red_flag_api.py
+++ backend/redflags/red_flag_api.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-import pandas as pdfrom fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
 import pandas as pd
 import json
 import time
